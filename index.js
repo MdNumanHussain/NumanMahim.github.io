fe65b108b30e6dd95195a6a8eb19ce19d3746100
Md Numan Hussain <mh21adq@herts.ac.uk>
2024-05-10 17:44:22 +0100

    css and js added for home page

---
+++ index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript code to run after the document is loaded
+    console.log("Website fully loaded and ready to go!");
+});
