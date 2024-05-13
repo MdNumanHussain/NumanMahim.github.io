f1a750d76d9f3a6d7656fa15d3fe31f6098a4385
Md Numan Hussain <mh21adq@herts.ac.uk>
2024-05-13 14:46:14 +0100

    Home pages recreated with new design

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // JavaScript code to run after the document is loaded
-    console.log("Website fully loaded and ready to go!");
+    console.log("Website loaded!");
 });
