609c45160bf2d4858879e669df6fb04d378c7b7f
Md Numan Hussain <mh21adq@herts.ac.uk>
2024-06-13 14:54:34 +0100

    Dark Mode and Light Mode added

--- nav.js
+++ nav.js
@@ -1,14 +1,35 @@
 function openMenu() {
     const sidemenu = document.getElementById("sidemenu");
-    sidemenu.style.right = "0"; // Adjust as per your CSS
-    sidemenu.classList.add("active"); // Adds 'active' class
+    sidemenu.style.right = "0";
+    sidemenu.classList.add("active");
 }
 
 function closeMenu() {
     const sidemenu = document.getElementById("sidemenu");
-    sidemenu.style.right = "-100%"; // Adjust as per your CSS
-    sidemenu.classList.remove("active"); // Removes 'active' class
+    sidemenu.style.right = "-100%";
+    sidemenu.classList.remove("active");
 }
+
+function toggleAccessibility() {
+    const body = document.body;
+    const modeSwitch = document.getElementById("mode-switch");
+    const modeText = document.getElementById("mode-text");
+
+    body.classList.toggle("accessibility-mode");
+
+    if (body.classList.contains("accessibility-mode")) {
+        modeSwitch.checked = true;
+        modeText.innerHTML = "Daymode";
+        modeText.classList.add("right");
+        modeText.classList.remove("left");
+    } else {
+        modeSwitch.checked = false;
+        modeText.innerHTML = "Nightmode";
+        modeText.classList.add("left");
+        modeText.classList.remove("right");
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('nav ul li a');
     const currentUrl = window.location.href;
